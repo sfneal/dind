da5fb4d3e76767cdbb1989b7b78f3fc7bf47e62f
mrstephenneal <stephen.neal14@gmail.com>
2019-07-03 19:04:57 -0400

    Update command.sh

--- command.sh
+++ command.sh
@@ -4,4 +4,4 @@
 sleep 15;
 
 # Get names of docker containers with 'nginx' in name
-sudo docker exec $(sudo docker ps --format {{.Names}} | grep ${nginx-container-name}) service nginx reload
+sudo docker exec $(sudo docker ps --format {{.Names}} | grep webserver) service nginx reload
