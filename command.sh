9da6385312a92cb2612922d45427924aa0a05949
mrstephenneal <stephen.neal14@gmail.com>
2019-07-08 10:52:11 -0400

    ADD docker version display

--- command.sh
+++ command.sh
@@ -3,5 +3,8 @@
 # Wait for the certbot to finish ACME challenge
 sleep 15;
 
+# Show Docker version
+sudo docker -v
+
 # Get names of docker containers with 'nginx' in name
 sudo docker exec $(sudo docker ps --format {{.Names}} | grep webserver) service nginx reload
