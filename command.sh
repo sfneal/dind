e8437c32dc8dad0bb9faea4f3d87d005da1ce9ce
mrstephenneal <stephen.neal14@gmail.com>
2019-07-08 10:55:08 -0400

    FIX docker version display

--- command.sh
+++ command.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Wait for the certbot to finish ACME challenge
-sleep 15;
-
 # Show Docker version
 sudo docker -v
+
+# Wait for the certbot to finish ACME challenge
+sleep 15;
 
 # Get names of docker containers with 'nginx' in name
 sudo docker exec $(sudo docker ps --format {{.Names}} | grep webserver) service nginx reload
