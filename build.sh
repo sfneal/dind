693044222d6b3330ba789c05b2e90ff086e92c64
mrstephenneal <stephen.neal14@gmail.com>
2019-07-09 14:58:59 -0400

    ADD alpine-3.9.4-v2 to build and push scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t stephenneal/dind:alpine-3.9.4-v1 ./alpine-3.9.4-v1/
+docker build -t stephenneal/dind:alpine-3.9.4-v2 ./alpine-3.9.4-v2/
 docker build -t stephenneal/dind:ubuntu-19.10-v1 ./ubuntu-19.10-v1/
--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docker push stephenneal/dind:alpine-3.9.4-v1
+docker push stephenneal/dind:alpine-3.9.4-v2
 docker push stephenneal/dind:ubuntu-19.10-v1
