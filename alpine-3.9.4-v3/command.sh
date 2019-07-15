f9d67f0e98d4bca86697a451412f98813c3619a6
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 09:25:45 -0400

    MAKE dind:alpine-3.9.4-v3 image

---
+++ alpine-3.9.4-v3/command.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Show Docker version
+sudo docker -v
+
+# TODO: replace with check to see if certbot container has stopped
+# Wait for the certbot to finish ACME challenge
+sleep 15;
+
+# Get names of docker containers with 'nginx' in name
+container_id="$(sudo docker ps -qf "name=${nginx_container_name}")"
+
+# Issue reload command to nginx webserver container
+sudo docker exec "${container_id}" sh /scripts/reload.sh
