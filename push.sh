fffdd9f6365fec62e5dc5e770097e792b357d6e2
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 09:27:07 -0400

    FIX build and push scripts

--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@
 
 docker push stephenneal/dind:alpine-3.9.4-v1
 docker push stephenneal/dind:alpine-3.9.4-v2
+docker push stephenneal/dind:alpine-3.9.4-v3
 docker push stephenneal/dind:ubuntu-19.10-v1
