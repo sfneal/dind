753c6d11e3f4d67e6934a9de2d319d08df868596
mrstephenneal <stephen.neal14@gmail.com>
2019-07-23 16:31:35 -0400

    ADD while loop to confirm that the certbot has exited

--- alpine-3.9.4-v4/scripts/entrypoint.sh
+++ alpine-3.9.4-v4/scripts/entrypoint.sh
@@ -2,31 +2,20 @@
 
 # Show Docker version
 sudo docker -v
+sleep 20
 
-# TODO: replace with check to see if certbot container has stopped
 # Wait for the certbot to finish ACME challenge
 while true; do
-    sleep 2
     is_waitfor_container_running=$(sh /scripts/is_running.sh)
     if [[ ${is_waitfor_container_running} == false ]]; then
         break
     else
-        echo "${wait_for_container} is still running... waiting 2 secs then checking again..."
+        echo "${wait_for_container} is still running... waiting 5 secs then checking again..."
+        sleep 5
     fi
 done
-sleep 30;
 
 # Execute Docker command on each container
 for cn in ${container_name}; do
     sh /scripts/exec-cmd.sh ${cn} ${container_cmd}
 done
-
-sleep 60;
-
-# Execute Docker command on each container
-for cn in ${container_name}; do
-    sh /scripts/exec-cmd.sh ${cn} ${container_cmd}
-done
-
-# Keep alive for a minute
-sleep 60;
