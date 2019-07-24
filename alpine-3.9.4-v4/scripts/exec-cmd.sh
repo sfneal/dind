5ec96641de1bc4c459b2dc1044ab83cab7254808
mrstephenneal <stephen.neal14@gmail.com>
2019-07-24 10:09:18 -0400

    ADD while loop with conditionals to resolve container names

--- alpine-3.9.4-v4/scripts/exec-cmd.sh
+++ alpine-3.9.4-v4/scripts/exec-cmd.sh
@@ -3,18 +3,25 @@
 container_name=${1}
 container_cmd=${2}
 
-# Get names of docker containers with 'nginx' in name
-container_id="$(sudo docker ps -aq --format {{.Names}} | grep ${container_name})"
-
-# Confirm that the container_id is not empty
-if [[ ! ${container_id} -ge 0 ]]; then
-    # Try removing the '-' char to resolve the container ID
-    container_name=$(echo "${1//-}")
+while true; do
+    # Get names of docker containers with 'nginx' in name
     container_id="$(sudo docker ps -aq --format {{.Names}} | grep ${container_name})"
+
+    # Confirm that the container_id is not empty
+    if [[ ${#container_id} == 0 ]]; then
+        # Try removing the '-' char to resolve the container ID
+        container_name=${container_name//-}
+    else
+        break
+    fi
+done
+
+# Check to see that the container is running before executing command
+if [[ $(sh /scripts/is_running.sh ${container_name}) == true ]]; then
+    echo "Preparing to execute command on container with the ID '${container_id}'..."
+
+    # Issue reload command to nginx webserver container
+    sudo docker exec "${container_id}" bash "${container_cmd}"
+else
+    echo "Skipping command execution on container with the ID '${container_id}' because it is not running..."
 fi
-
-# Display ID of container we're about to execute a command on
-echo "Preparing to execute command on container with the ID '${container_id}'..."
-
-# Issue reload command to nginx webserver container
-sudo docker exec "${container_id}" bash "${container_cmd}"
