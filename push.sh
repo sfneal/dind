2e573b13e23748a605bb65f34cd73f0970e7d86f
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:35:42 -0500

    ADD absolute directory resolving to build and push shell scripts

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sh ./build.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sh "${DIR}"/build.sh
 
 docker push stephenneal/dind:alpine-3.9.4-v1
 docker push stephenneal/dind:alpine-3.9.4-v2
