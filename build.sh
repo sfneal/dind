78edaa177a1002452edc266dc10717fa7770e74b
mrstephenneal <stephen.neal14@gmail.com>
2020-07-07 13:48:03 -0400

    FIX build & push scripts to include new images

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ docker build -t stephenneal/dind:alpine-3.9.4-v2 "${DIR}"/alpine-3.9.4-v2/
 docker build -t stephenneal/dind:alpine-3.9.4-v3 "${DIR}"/alpine-3.9.4-v3/
 docker build -t stephenneal/dind:alpine-3.9.4-v4 "${DIR}"/alpine-3.9.4-v4/
 docker build -t stephenneal/dind:alpine-3.10-v1 "${DIR}"/alpine-3.10-v1/
+docker build -t stephenneal/dind:alpine-3.12-v1 "${DIR}"/alpine-3.12-v1/
 docker build -t stephenneal/dind:ubuntu-19.10-v1 "${DIR}"/ubuntu-19.10-v1/
--- push.sh
+++ push.sh
@@ -9,4 +9,5 @@ docker push stephenneal/dind:alpine-3.9.4-v2
 docker push stephenneal/dind:alpine-3.9.4-v3
 docker push stephenneal/dind:alpine-3.9.4-v4
 docker push stephenneal/dind:alpine-3.10-v1
+docker push stephenneal/dind:alpine-3.12-v1
 docker push stephenneal/dind:ubuntu-19.10-v1
